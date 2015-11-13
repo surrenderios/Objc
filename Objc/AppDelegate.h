2bcee7d732412fc11393394b1f76533bd9f49b94
Alex <alexwuyujing@gmail.com>
2015-11-13 16:54:53 +0800

    Initial Commit

---
+++ Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objc
+//
+//  Created by Alex on 15/11/13.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objc
+//
+//  Created by Alex on 15/11/13.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
